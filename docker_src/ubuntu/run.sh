0a72888f758bd7ec3d1bb65ac24d58f11526a94f
Matt Curfman <matt@tecnovelty.com>
2020-01-08 11:25:24 -0800

    Update run.sh

--- docker_src/ubuntu/run.sh
+++ docker_src/ubuntu/run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 docker build -t openvino_src_x64 .
+
 git clone https://github.com/opencv/dldt /tmp/dldt
+pushd /tmp/dldt
+git submodule init
+git submodule update
+popd
 
 # Build default version of openvino runtime
 docker run -it --rm -v /tmp/dldt:/dldt openvino_src_x64 bash -c 'cd /dldt/inference-engine && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. &&   make --jobs=$(nproc --all)' 
