c3be2d9bc74ec7d90f8ae2c116b8513e37976020
Matt Curfman <matt@tecnovelty.com>
2020-01-08 04:11:25 +0000

    Added support for building from src
    Signed-off-by: Matt Curfman <matt@tecnovelty.com>

---
+++ docker_src/ubuntu/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker build -t openvino_src_x64 .
+git clone https://github.com/opencv/dldt /tmp/dldt
+
+# Build default version of openvino runtime
+docker run -it --rm -v /tmp/dldt:/dldt openvino_src_x64 bash -c 'cd /dldt/inference-engine && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. &&   make --jobs=$(nproc --all)' 
