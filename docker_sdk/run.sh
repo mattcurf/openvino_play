cab3564e91fe840cfedaa05f20b2802fdda2584d
Matt Curfman <matt@tecnovelty.com>
2020-01-08 15:23:01 -0800

    Update run.sh

--- docker_sdk/run.sh
+++ docker_sdk/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker build -t openvino .
-docker run -it --rm --device /dev/dri:/dev/dri openvino bash -c 'cd /opt/intel/openvino/deployment_tools/demo && ./demo_benchmark_app.sh -d CPU && ./demo_benchmark_app.sh -d GPU'
+docker run -it --rm openvino bash -c 'cd /opt/intel/openvino/deployment_tools/demo && ./demo_benchmark_app.sh -d CPU'
+docker run -it --rm --device /dev/dri:/dev/dri openvino bash -c 'cd /opt/intel/openvino/deployment_tools/demo && ./demo_benchmark_app.sh -d GPU'
