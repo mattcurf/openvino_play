2c516beeb83d9646562d931294077a962268a237
Matt Curfman <matt@tecnovelty.com>
2020-06-08 17:58:23 -0700

    Fixes

--- compute_runtime/run.sh
+++ compute_runtime/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 docker build -t compute-runtime .
-
-docker run -it --rm --net=host --privileged --device /dev/dri:/dev/dri compute-runtime
+docker run -it --rm --device /dev/dri:/dev/dri compute-runtime bash -c "/tmp/level-zero-tests/out/ze_bandwidth"
 
