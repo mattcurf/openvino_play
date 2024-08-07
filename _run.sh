0f1231765eb56a807d22461429c640d6abcff8cf
Matt Curfman <matt@tecnovelty.com>
2024-08-07 07:32:57 -0700

    Add OpenVINO aarch64 support

--- _run.sh
+++ _run.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 source /opt/intel/openvino_2024.3.0/setupvars.sh
 
+ARCH=$(uname -i)
+if [ "$ARCH" == 'x86_64' ];
+then 
+  ARCH=intel64
+fi
+
 # Run query
-/root/openvino_cpp_samples_build/intel64/Release/hello_query_device
+/root/openvino_cpp_samples_build/$ARCH/Release/hello_query_device
 
 # Run benchmark
 python3 ./download_resnet50.py
 
-/root/openvino_cpp_samples_build/intel64/Release/benchmark_app -m model/ir_model/resnet50_fp16.xml -d CPU -hint throughput -niter 1000
+/root/openvino_cpp_samples_build/$ARCH/Release/benchmark_app -m model/ir_model/resnet50_fp16.xml -d CPU -hint throughput -niter 1000
 
 if [ "$HAS_GPU" == "1" ]; then
-  /root/openvino_cpp_samples_build/intel64/Release/benchmark_app -m model/ir_model/resnet50_fp16.xml -d GPU -hint throughput -niter 1000
+  /root/openvino_cpp_samples_build/$ARCH/Release/benchmark_app -m model/ir_model/resnet50_fp16.xml -d GPU -hint throughput -niter 1000
 fi
 
 if [ "$HAS_NPU" == "1" ]; then
-  /root/openvino_cpp_samples_build/intel64/Release/benchmark_app -m model/ir_model/resnet50_fp16.xml -d NPU -hint throughput -niter 1000
+  /root/openvino_cpp_samples_build/$ARCH/Release/benchmark_app -m model/ir_model/resnet50_fp16.xml -d NPU -hint throughput -niter 1000
 fi
